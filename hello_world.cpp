e8c7680b7e0c989745c13f4f458dfb317f099332
mongilyov <andrey1987697@gmail.com>
2023-02-28 22:09:59 +0300

    fixed code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main() {
     std::string s;
-    cout << "Your name is: ";
-    cin >> s;
-    cout << "Hello world from " << s << endl;
+    std::cout << "Your name is: ";
+    std::cin >> s;
+    std::cout << "Hello world from " << s << std::endl;
+    return 0;
 }
